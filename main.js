ce69a2260511dd59484e41cabd54862dd48fda76
Brayden Grotegut <bgrotegut21@gmail.com>
2021-07-15 01:28:55 -0600

    working on calculator

--- main.js
+++ main.js
@@ -31,28 +31,86 @@ function checkLength(){
 }
 
 function clearScreen () {
-    screenText.textContent = "";
+    screenText.textContent = 0;
+    showAnwser = true;
     numbersArray = [];
 }
 
-function addition(){
-    if (numbersArray != 2){
-        numbersArray.push(Number(screenText.textContent));
-        showAnwser = true
+
+function pushNumbers(type){
+    if (showAnwser) return;
+    let expression;
+    if (numbersArray.length < 1){
+        expression = {number: screenText.textContent, operation:type}
+    } else {
+        numbersArray.map(value => {
+            if (value.operation != "none"){
+                expression = {number: screenText.textContent, operation:"none"}
+            } else {
+                console.log(type)
+                expression = {number: screenText.textContent, operation:type}
+            }
+        })
     }
 
+    numbersArray.push(expression);
+    showAnwser = true;
+    screenText.textContent = "";
+    operator();    
 
-    if (numbersArray.length == 2){
-        let anwser = numbersArray[0] + numbersArray[1];
-        
-        screenText.textContent = anwser;
-        numbersArray = [anwser]
+}
 
+
+
+
+
+
+
+function evaluateExpression(array){
+
+    let anwser = 0;
+
+    array.map (value => {
+        console.log(value)
+        if (value.operation == "addition"){
+
+            anwser = Number(numbersArray[0].number) + Number(numbersArray[1].number);
+            
+            
+        }
+        if (value.operation == "multiply"){
+             anwser = Number(numbersArray[0].number) * Number(numbersArray[1].number);
+        }
+
+
+    })
+    let finalAnwser = {number: anwser, operation: "none"};
+    console.log(finalAnwser);
+    numbersArray = [finalAnwser]
+
+
+    screenText.textContent = finalAnwser.number;
+    if (screenText.textContent.length > 12){
+        let number = Number(screenText.textContent);
+        let roundedNumber = number.toExponential(8);
+        screenText.textContent = roundedNumber;
     }
 }
 
 
-function operator () {}
+
+function operator () {
+    console.log(numbersArray)
+    if(numbersArray.length  < 1) return;
+    if (numbersArray.length != 2){
+        let expression = {number: Number(screenText.textContent), operation: "none"}
+        numbersArray.push(expression)
+    }
+    if (numbersArray.length == 2){
+        evaluateExpression(numbersArray)
+    }
+
+}
 
 
 function addDecimal(){  
@@ -82,26 +140,35 @@ let numberButtons = [{button:one,number:1},{button:two,number:2},
                     {button:seven,number:7},{button:eight,number:8},
                     {button:nine,number:9}, {button: zero, number: 0}];
 
-let operateButtons = [{button:plus, execute: addition}, {button:equals, execute: operator}]
+let operateButtons = [{button:plus, execute: pushNumbers, type: "addition"}, {button:equals, execute: operator},
+                      {button:multiply, execute: pushNumbers, type: "multiply"}
+
+
+]
 
                     
 numberButtons.map(numberButton => {
     numberButton.button.addEventListener("click", () => {
+        checkLength();
         if(canAddNumber){
             if (showAnwser){
                 screenText.textContent = "";
+
                 showAnwser = false;
             }
             screenText.textContent += numberButton.number;
-            console.log(numbersArray)
+
+            
         }
-        checkLength();
     })
 })
 
 function executeButtons(buttn){
     buttn.map(calcButton => {
         calcButton.button.addEventListener("click", () => {
+            if (calcButton.type){
+                calcButton.execute(calcButton.type)
+            }
             calcButton.execute();
         })
     })
